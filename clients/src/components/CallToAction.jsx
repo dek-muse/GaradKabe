0dd6775f0b1ea6bb97662d998e11d33226e7b72e
dek-muse <mohdeeqibnuipraahim@gmail.com>
2024-06-24 12:27:55 +0300

    done

--- clients/src/components/CallToAction.jsx
+++ clients/src/components/CallToAction.jsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { Button } from 'flowbite-react';
  
- 
 import goal from '../assets/goal.jpg'
 import sotry from '../assets/sotry.jpg'
 import gym from '../assets/gym.jpeg'
@@ -11,38 +10,36 @@ import { Link } from 'react-router-dom';
 
 const CallToAction = () => {
   return (
-   <div className='flex flex-col sm:flex-row p-3   justify-center items-center rounded-br-3xl text-center'>
-    <div className="flex-4 justify-center flex ">
- 
-            {/* <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-            <Link
-            to='/search/guulaha'
-            className='text-lg text-white hover:underline text-center'
-          >
-            successful story
-          </Link>
-            </Button> */}
-  
-            <div className="p-7 flex-1">
-    <img src={goal} className='w-96 ml-18 rounded-lg'/>
+    <div className='flex flex-col sm:flex-row p-3 justify-center items-center rounded-br-3xl text-center'>
+      <div className="flex flex-col sm:flex-row justify-center flex-wrap">
+        <div className="p-3 sm:p-7 flex-1 relative group">
+          <img src={goal} className='w-full sm:w-96 rounded-lg'/>
+          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center text-white text-lg rounded-lg">
+            Goal Achievement
+          </div>
+        </div>
+        <div className="p-3 sm:p-7 flex-1 relative group">
+          <img src={sotry} className='w-full sm:w-96 rounded-lg'/>
+          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center text-white text-lg rounded-lg">
+            Success Story
+          </div>
+        </div>
+        <div className="p-3 sm:p-7 flex-1 relative group">
+          <img src={gym} className='w-full sm:w-96 rounded-lg'/>
+          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center text-white text-lg rounded-lg">
+            Gym Workout
+          </div>
+        </div>
+        <div className="p-3 sm:p-7 flex-1 relative group">
+          <img src={moti} className='w-full sm:w-96 rounded-lg'/>
+          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex justify-center items-center text-white text-lg rounded-lg">
+            Motivation
+          </div>
+        </div>
+      </div>
     </div>
- 
-            <div className="p-7 flex-1">
-    <img src={sotry} className='w-96 ml-18 rounded-lg'/>
-    </div>
- 
-            <div className="p-7 flex-1">
-    <img src={gym} className='w-96 ml-18 rounded-lg'/>
-    </div>
- 
-            <div className="p-7 flex-1">
-    <img src={moti} className='w-96 ml-18 rounded-lg'/>
-    </div>
- 
-
-    </div>
-   </div>
   )
 }
 
-export default CallToAction
+export default CallToAction;
+        
